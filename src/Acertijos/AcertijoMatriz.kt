c81466ecc087f3827e47338bd8d2f14bab45e0c8
JosGon615 <josgon615@alu.centroafuera.es>
2022-10-13 14:13:58 +0200

    6

--- src/Acertijos/AcertijoMatriz.kt
+++ src/Acertijos/AcertijoMatriz.kt
@@ -1,41 +1,64 @@
 package Acertijos
 
 fun main () {
-    var matriz: String =
-            "1 2 3\n " + //arraylist (1 2 3)
-            "4 5 6\n " + //arraylist
-            "7 8 9\n"
-
     var contF = 0
-    var contC = 1
-    var cont = 0
+    var contC = 0
+    var i = 0
 
+    val arrayG = arrayOf<String>("1","2","3","\n","4","5","6","\n","7","8","9","\n","7","8","9","\n")
 
-    val arrayG = arrayOf<String>("1","2","3","\n","4","5","6","\n","7","8","9")
 
     arrayG.forEach {
-        print("\t")
-        if (it != "\n" || cont<=contF){
-            print(it)
-            print(" ")
-            cont++
-        }
-
-
-        if(it == "\n"){
+        if (it == "\n") {
             contF++
-            print(contF)
         }
+        else {
+            contC++
+        }
+    }
+    contC= contC/contF
+    println("contador F")
+    println(contF)
+    println("contador C")
+    println(contC)
 
 
+    //print contador columnas
+    print("   ")
+    repeat(contF){
+        print(contC)
+        contC++
+        repeat(2){
+            print(" ")
+        }
+    }
 
-
+    //print guiones
+    println()
+    print(" |")
+    repeat(contF*3){
+        print("-")
     }
 
 
+    println()
 
+    i=1
+    print(contC)
+    print("| ")
 
-
-
+    arrayG.forEach {
+        //print contador filas
+       if(it != "\n"){
+           print(it)
+           print("  ")
+       }
+       else{
+           i++
+           println()
+           print(i)
+           print("| ")
+        }
+    }
 
 }
